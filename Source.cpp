ee2a09765513bca2fc94067b3bc0818eba1e70a8
Utkarsh Gupta <500075374@stu.upes.ac.in>
2020-09-15 23:12:16 +0530

    Initialialising files with proper comments.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,17 @@
+// Source.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// The program is coded by Utkarsh Gupta @AvGeekGupta
 
+// Including header files
+#include <iostream>
+#include "linkedlist.h"
 
+// Including namespaces
+using namespace std;
+
+// 'main' function : Program execution begins and ends here.
 int main(void) {
+	// Execution starts.
+
+	// Execution ends.
 	return 0;
 }
--- linkedlist.h
+++ linkedlist.h
@@ -1,13 +1,21 @@
+// linkedlist.h : This file contains three classes, 'singlyLinkedList', 'doublyLinkedList' & 'circularLinkedList'.
+// The program is coded by Utkarsh Gupta @AvGeekGupta
 #pragma once
 
+/*
+*/
 class singlyLinkedList {
 
 };
 
+/*
+*/
 class doublyLinkedList {
 
 };
 
+/*
+*/
 class circularLinkedList {
 
 };
